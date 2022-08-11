114358b5e2790b4b1dd1b254ff174e3ae2a4304a
Daniel Card <dan.card@xservus.com>
2022-08-11 22:37:01 +0100

    Update SinkHoldeTLD.ps1

--- SinkHoldeTLD.ps1
+++ SinkHoldeTLD.ps1
@@ -1,4 +1,4 @@
-$badtld = Get-Content C:\dns\blocklist\malic-dns-tld.txt
+$badtld = Get-Content malic-dns-tld.txt
 
 $badtld = $badtld | Sort-Object -Unique
 
@@ -11,7 +11,7 @@ $domainname
 
 Add-DnsServerPrimaryZone -Name $domainname.tostring() -ReplicationScope "Forest" -PassThru
 
-Add-DnsServerResourceRecordA -Name "*" -ZoneName $domainname.tostring() -IPv4Address "192.168.1.1" -TimeToLive 01:00:00
+Add-DnsServerResourceRecordA -Name "*" -ZoneName $domainname.tostring() -IPv4Address "127.0.0.1" -TimeToLive 01:00:00
 
 #remove domain zone
 #Remove-DnsServerZone -Name $domainname.tostring() -Force
